376e5478bb2c4b71b51f77b27816cb97fc9ec2b3
Xcatgit0 <163300721+Xcatgit0@users.noreply.github.com>
2024-04-05 06:05:35 +0700

    ll

--- setup.lua
+++ setup.lua
@@ -15,7 +15,7 @@ if #args > 0 then
     if args[1] == "list" then
         action = shell.execute("cat /home/psetup/plist.txt")
     elseif args[1] == "install" then
-        if args[2] == DrawX then
+        action = if args[2] == DrawX then
             action = shell.execute("cp /home/psetup/.asset/DrawX.lua /usr/bin/DrawX.lua")
         end
     end
