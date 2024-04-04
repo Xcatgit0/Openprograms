5c4733131345a4ce06915c10e88a37888051a52b
Xcatgit0 <163300721+Xcatgit0@users.noreply.github.com>
2024-04-05 06:07:03 +0700

    wow

--- .asset/DrawX.lua
+++ .asset/DrawX.lua
@@ -1,6 +1,7 @@
 local component = require("component")
 local event = require("event")
 local gpu = component.gpu
+require("tty").clear()
 while true do
     local a, b, x, y = event.pull("touch")
     gpu.set(x, y,"X")
