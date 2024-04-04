be0de98cd8d1409532e458e756f9d51a57bf7921
Xcatgit0 <163300721+Xcatgit0@users.noreply.github.com>
2024-04-04 14:44:13 +0700

    wow

--- setup.lua
+++ setup.lua
@@ -2,3 +2,8 @@ local component = require("component")
 local gpu = component.gpu
 local shell = require("shell")
 local event = require("event")
+local args = shell.parse(...)
+if #args > 1 then
+    print("Use: setup (text)")
+end
+print(args[1])
